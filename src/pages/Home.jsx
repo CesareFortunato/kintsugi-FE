8e66ce7c65f14137cf76aa19aabdfa4eaf01691b
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2026-03-09 16:41:36 +0100

    Struttura homepage e product card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,34 @@
-export default function Home(){
+import ProductCard from "../components/ProductCard"
+import { useState,useEffect } from "react"
+const endpoint = ''
 
+export default function Home(){
+const [products,setProduct]= useState ([])
+//funzione chiamata axios
+const fetchProduct =()=>{
+    axios.get(endpoint)
+    .then (res=>{setProduct(res.data)})
+    .catch(err=>{console.log('errore')})
+}
+//funzione di rendering li stato dei prodotti 
+const renderProduct =()=>{
+    return products.map (product=>{
+        return (
+            <div className="col" key={product.id}>
+                <ProductCard product ={product}  />
+            </div>
+        )
+    })
+}
+//richiamo funzione di fetch al montaggio della page
+useEffect(fetchProduct,[])
 return(
+    <>
 <h1>Home</h1>
+<div className="row row-cols-3 mt-d">
+       {renderProduct()}         
+    </div>
+    </>
 )
 
 }
