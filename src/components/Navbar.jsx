690346dfcbf08452963318dab1b4a8f8ccf87eff
serenileonardo <serenileonardo.web@gmail.com>
2026-03-09 14:27:49 +0100

    fix nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function Navbar() {
   return (
-    <nav style={{textAlign:"center", display:"flex", justifyContent:"space-around"}}>
+    <nav className="navbar" style={{textAlign:"center", display:"flex", justifyContent:"space-around"}}>
       <Link to="/">Home</Link>
       <Link to="/products">Prodotti</Link>
       <Link to="/wishlist">Preferiti</Link>
