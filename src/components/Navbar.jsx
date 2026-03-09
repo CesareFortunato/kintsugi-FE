16d7dc0de958d05184c4f9dd4233eaeca66b889e
serenileonardo <serenileonardo.web@gmail.com>
2026-03-09 13:07:50 +0100

    feat improve footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function Navbar() {
   return (
-    <nav style={{textAlign:"center"}}>
+    <nav style={{textAlign:"center", display:"flex", justifyContent:"space-around"}}>
       <Link to="/">Home</Link>
       <Link to="/products">Products</Link>
       <Link to="/wishlist">Wishlist</Link>
