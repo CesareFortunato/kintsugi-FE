9e6c5bac96925376650b4d37bdd04b40917cddb5
CesareFortunato <cesare.fortunato89@gmail.com>
2026-03-10 16:51:09 +0100

    feat: addtocart added to productCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom";
+import { addToCart } from "../utils/cart";
 
 function ProductCard({ product }) {
     const { name, description, price, size_ml, product_image_url, public_slug } = product;
+
+    const handleAddToCart = () => {
+        addToCart(product);
+        alert("Prodotto aggiunto al carrello");
+    };
 
     return (
         <div className="card h-100" style={{ width: "18rem" }}>
@@ -25,6 +31,10 @@ function ProductCard({ product }) {
                 <Link to={`/products/${public_slug}`} className="btn btn-dark">
                     Vai al dettaglio
                 </Link>
+
+                <button className="btn btn-dark" onClick={handleAddToCart}>
+                    Add to Cart
+                </button>
             </div>
         </div>
     );
