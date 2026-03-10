c2fd972dfc69dac76ced0dcef10df596622ac870
CesareFortunato <cesare.fortunato89@gmail.com>
2026-03-10 11:49:42 +0100

    fix: routes + public_slug + endpoint fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from "./pages/Home"
-import Products from "./pages/Products"
-import ProductPage from "./pages/ProductPage"
-import Wishlist from "./pages/Wishlist"
-import Cart from "./pages/Cart"
-import DetailPage from "./pages/DetailPage"
-
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import DetailPage from "./pages/DetailPage";
+import Wishlist from "./pages/Wishlist";
+import Cart from "./pages/Cart";
 
 function App() {
-
-    return (
-
-
-        <BrowserRouter>
-            <Routes>
-
-                <Route path="/" element={<Home />} />
-
-                <Route path="/products" element={<Products />} />
-
-                <Route path="/products/:id" element={<DetailPage />} />
-
-<Route path="/products/:id" element={<DetailPage />} />
-
-                <Route path="/cart" element={<Cart />} />
-
-            </Routes>
-        </BrowserRouter>
-
-
-
-    )
-
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:public_slug" element={<DetailPage />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,26 +1,33 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-function ProductCard(props) {
-    const { id, name, description, price, size_ml, product_image_url } = props.product
+function ProductCard({ product }) {
+    const { name, description, price, size_ml, product_image_url, public_slug } = product;
+
     return (
-    
-        <div className="card" style="width: 18rem;">
-            <img src={product_image_url} className={name} alt="..."/>
-                <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
-                    <p className="card-text">{description}</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li className="list-group-item">{price}</li>
-                    <li className="list-group-item">{size_ml}</li>
-                </ul>
-                <div className="card-body">
-                    <Link to={`products/${id}`}></Link>
-                </div>
+        <div className="card h-100" style={{ width: "18rem" }}>
+            <img
+                src={product_image_url}
+                className="card-img-top"
+                alt={name}
+            />
+
+            <div className="card-body">
+                <h5 className="card-title">{name}</h5>
+                <p className="card-text">{description}</p>
+            </div>
+
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">Prezzo: €{price}</li>
+                <li className="list-group-item">Formato: {size_ml} ml</li>
+            </ul>
+
+            <div className="card-body">
+                <Link to={`/products/${public_slug}`} className="btn btn-dark">
+                    Vai al dettaglio
+                </Link>
+            </div>
         </div>
-
-    )
-
+    );
 }
 
-export default ProductCard
+export default ProductCard;
