470e6579bb23fc3c1a1cfb4cbe28aff2ff666b27
CesareFortunato <cesare.fortunato89@gmail.com>
2026-03-11 17:41:36 +0100

    feat: populate products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,48 @@
-export default function Products(){
+import ProductCard from "../components/ProductCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-return(
-<h1>Products</h1>
-)
+const endpoint = "http://localhost:3000/parfumes";
+
+export default function Products() {
+
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = () => {
+        axios
+            .get(endpoint)
+            .then((res) => {
+                setProducts(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    return (
+        <>
+            
+
+            <div className="container my-5">
+
+                <h2 className="mb-4 text-center">Products</h2>
+
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+
+                    {products.map((product) => (
+                        <div className="col" key={product.id}>
+                            <ProductCard product={product} />
+                        </div>
+                    ))}
+
+                </div>
+
+            </div>
+        </>
+    )
 
 }
