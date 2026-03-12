c238c71d5583cac22f86a08a8af1142b30010503
serenileonardo <serenileonardo.web@gmail.com>
2026-03-12 15:06:11 +0100

    feat: improve footer promo section and css

--- src/sections/PromoSection.jsx
+++ src/sections/PromoSection.jsx
@@ -25,11 +25,11 @@ setProducts(promo)
 
 return(
 
-<section className="container my-5">
+<section className="container  my-5">
 
 <h2 className="mb-4 text-center">Promotions</h2>
 
-<div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+<div className="row g-4 justify-content-center">
 
 {products.map(product =>(
 
