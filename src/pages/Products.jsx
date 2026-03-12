8baa9d0aad18da99feec8cfb1c4df021cb91d17f
serenileonardo <serenileonardo.web@gmail.com>
2026-03-12 16:27:13 +0100

    feat:change row

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -31,7 +31,7 @@ export default function Products() {
 
                 <h2 className="mb-4 text-center">Products</h2>
 
-                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                <div className="row  g-4">
 
                     {products.map((product) => (
                         <div className="col" key={product.id}>
