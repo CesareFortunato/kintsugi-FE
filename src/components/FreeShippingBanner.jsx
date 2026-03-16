125cd8739ebff091dc2ad108a65a52d8c7e77ab9
serenileonardo <serenileonardo.web@gmail.com>
2026-03-16 12:43:34 +0100

    fix color

--- src/components/FreeShippingBanner.jsx
+++ src/components/FreeShippingBanner.jsx
@@ -3,7 +3,7 @@ export default function FreeShippingBanner() {
     <div
       className="text-center py-1"
       style={{
-        backgroundColor: "#53d800",
+        backgroundColor: "#5dff48",
         fontSize: "0.85rem",
         letterSpacing: "0.5px",
         fontWeight: "500"
