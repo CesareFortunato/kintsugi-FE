bca91b0715be94d02678fb781e7cbabf4e1acd53
alessiaimprota <alessiaimprota126@gmail.com>
2026-03-18 16:02:54 +0100

    fix

--- src/sections/PromoSection.jsx
+++ src/sections/PromoSection.jsx
@@ -4,39 +4,30 @@ import ProductCard from "../components/ProductCard";
 
 const endpoint = "http://localhost:3000/parfumes";
 
-export default function PromoSection(){
+export default function PromoSection() {
+  const [products, setProducts] = useState([]);
 
-const [products,setProducts] = useState([]);
+  useEffect(() => {
+    axios.get(endpoint).then((res) => {
+      const promo = res.data.filter(
+        (product) => product.discount_value !== null,
+      );
 
-useEffect(()=>{
+      setProducts(promo);
+    });
+  }, []);
 
-axios.get(endpoint)
-.then(res=>{
+  return (
+    <section className="container py-4 my-5">
+      <h2 className="mb-4 text-center">Promozioni</h2>
 
-const promo = res.data.filter(
-product => product.discount_value !== null
-)
-
-setProducts(promo)
-
-})
-
-},[])
-
-return(
-
-<section className="container py-4 my-5">
-
-<h2 className="mb-4 text-center">Promozioni</h2>
-
-<div className="d-flex flex-wrap justify-content-center gap-4">
-  {products.map(product => (
-    <ProductCard key={product.id} product={product} />
-  ))}
-</div>
-
-</section>
-
-)
-
+      <div className=" justify-content-center gap-5 row flex-nowrap">
+        {products.map((product) => (
+          <div className="col-3">
+            <ProductCard key={product.id} product={product} />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
